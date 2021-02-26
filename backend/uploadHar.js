8db01e1703e9231efb6f94222a855e37130f66ba
Konstantinos Tsakonas <iamtsac@gmail.com>
2021-02-26 21:09:57 +0200

    unfinished harUpload.js backend

--- backend/uploadHar.js
+++ backend/uploadHar.js
@@ -3,25 +3,28 @@ export function uploadHar(app, connection) {
 
         //connection.query('INSERT INTO User (username, e_mail, passwd) VALUES (?, ?, ?)', [username, e_mail, password]);
 
-        var data = request.body; 
-        var entryList = [ "startedDateTime","serverIPAddress","wait" ];
-        var requsetList = ["url","method","hostRequest","pragmaRequest","cache-controlRequest"];
-        var responseList = ["status","statusText","cache-controlResponse","pragmaResponse","age","last-modifiedResponse","content-typeResponse","expiresResponse"];
-        for (var idx in data) { 
-            console.log(data[idx]);
-            entryList.forEach(function(element) {
-                data[idx][element];
-                //console.log('pragmaResponse'.replace('Response', ''))
-            })
-            requsetList.forEach(function(element) {
-                data[idx][element];
-                connection.query('INSERT INTO Request (entry_id, url, method) VALUES (?, ?, ?)', [username, e_mail, password])
-                //console.log('pragmaResponse'.replace('Response', ''))
-            })
-            responseList.forEach(function(element) {
-                data[idx][element];
-                //console.log('pragmaResponse'.replace('Response', ''))
-            })
-        } 
+        var data = request.body;
+        //var entryList = [ "startedDateTime","serverIPAddress","wait" ];
+        //var requestList = ["url","method","hostRequest","pragmaRequest","cache-controlRequest"];
+        //var responseList = ["status","statusText","cache-controlResponse","pragmaResponse","age","last-modifiedResponse","content-typeResponse","expiresResponse"];
+        var requestList = ["startedDateTime", "serverIPAddress", "wait", "url", "method", "hostRequest", "pragmaRequest", "cache-controlRequest", "status", "statusText", "cache-controlResponse", "pragmaResponse", "age", "last-modifiedResponse", "content-typeResponse", "expiresResponse"];
+        var nestedList = []
+        for (var idx in data) {
+            var tmp = [];
+
+            for (var element in requestList) {
+
+                let currentValue = data[idx][requestList[element]];
+
+                if (currentValue != null) {
+                    tmp[element] = data[idx][requestList[element]];
+                }
+                else tmp[element] = null;
+            }
+            nestedList[idx] = tmp
+        }
+        console.log(nestedList[590])
+        console.log(data[590])
+
     });
 }
